92c43c6fd851924714cc20be49c839d76c6f465a
porChe1223 <houshinri@gmail.com>
2024-10-22 21:06:45 +0900

    使用しないルートのコメントアウト(developからファイル削除した方が良いかも)

--- src/app/api/exams/uploaders/[uploaderId]/route.ts
+++ src/app/api/exams/uploaders/[uploaderId]/route.ts
@@ -1,30 +1,32 @@
-import { getCurrentUserId } from "@/app/lib/auth";
-import prisma from "@/app/lib/prisma";
-import { validationEditSchema } from '@/validationSchema';
-import { promises as fs } from 'fs';
-import f from 'fs/promises';
-import { NextRequest, NextResponse } from "next/server";
-import path from 'path';
-import { v4 as uuidv4 } from 'uuid';
+// import { getCurrentUserId } from "@/app/lib/auth";
+// import prisma from "@/app/lib/prisma";
+// import { validationEditSchema } from '@/validationSchema';
+// import { promises as fs } from 'fs';
+// import f from 'fs/promises';
+// import { NextRequest, NextResponse } from "next/server";
+// import path from 'path';
+// import { v4 as uuidv4 } from 'uuid';
 
-export async function GET(req: NextRequest, { params_exam }: { params_exam: { uploaderId: string } }) {
-    const { uploaderId } = params_exam;
+// export async function GET(req: NextRequest, { params }: { params: { uploaderId: string } }) {
+//     const { uploaderId } = params;
+//     console.log('getYourExamByUploaderIdルートからのparams確認')
+//     console.log(params);
 
-    const exams = await prisma.exam.findMany({
-        where: { uploaderId },
-        select: {
-            lecture: true,
-            department: true,
-            professor: true,
-        },
-    });
+//     const exams = await prisma.exam.findMany({
+//         where: { uploaderId },
+//         select: {
+//             lecture: true,
+//             department: true,
+//             professor: true,
+//         },
+//     });
 
-    console.log('examsの確認');
-    console.log(exams);
+//     console.log('getYourExamByUploaderIdルートからのexamsの確認');
+//     console.log(exams);
 
-    if (!exams) {
-        return new NextResponse(JSON.stringify({ message: 'まだ何も投稿していません' }), { status: 404 });
-    }
+//     if (!exams) {
+//         return new NextResponse(JSON.stringify({ message: 'まだ何も投稿していません' }), { status: 404 });
+//     }
 
-    return new NextResponse(JSON.stringify(exams), {status: 200});
-}
+//     return new NextResponse(JSON.stringify(exams), {status: 200});
+// }
