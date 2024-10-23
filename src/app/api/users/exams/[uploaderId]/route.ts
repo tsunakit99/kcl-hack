7305392e7479a5fd410fc7fae5e3cbfbd280358a
tsunakit99 <tainosuke0926@gmail.com>
2024-10-24 00:17:34 +0900

    fix: デバックログ削除

--- src/app/api/users/exams/[uploaderId]/route.ts
+++ src/app/api/users/exams/[uploaderId]/route.ts
@@ -1,13 +1,8 @@
-import { getCurrentUserId } from "@/app/lib/auth";
 import prisma from "@/app/lib/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: { uploaderId: string } }) {
     const { uploaderId } = params;
-    console.log('getUserByIdルートからのparams確認')
-    console.log(params);
-    console.log('getUserByIdルートからのuploaderId確認')
-    console.log(uploaderId);
     const exams = await prisma.exam.findMany({
         where: { uploaderId },
         select: {
@@ -22,17 +17,12 @@ export async function GET(req: NextRequest, { params }: { params: { uploaderId: 
         },
     });
 
-    console.log('getUserByIdルートからのexamsの確認');
-    console.log(exams);
-
     const formattedExams = exams.map(exam => ({
         lectureName: exam.lecture.name, // lectureのnameを抽出
         departmentName: exam.department.name, // departmentのnameを抽出
         professor: exam.professor, // professorそのまま
         year: exam.year,
     }));
-    console.log('formattedExamsの確認');
-    console.log(formattedExams);
 
     if (!formattedExams) {
         return new NextResponse(JSON.stringify({ message: 'まだ何も投稿していません' }), { status: 404 });
