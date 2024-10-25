bb2221ff5341be91537568e929bed3f789751bed
tsunakit99 <tainosuke0926@gmail.com>
2024-10-25 21:23:17 +0900

    fix: ビルドエラー

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,14 +1,21 @@
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = global as unknown as {
+//   prisma: PrismaClient | undefined;
+// };
+
+// // 既にグローバルに存在しない場合のみ新しいPrismaClientを作成
+// if (!globalForPrisma.prisma) {
+//   globalForPrisma.prisma = new PrismaClient();
+// }
+
+// const prisma = globalForPrisma.prisma;
+
+// export default prisma;
+
+// prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = global as unknown as {
-  prisma: PrismaClient | undefined;
-};
-
-// 既にグローバルに存在しない場合のみ新しいPrismaClientを作成
-if (!globalForPrisma.prisma) {
-  globalForPrisma.prisma = new PrismaClient();
-}
-
-const prisma = globalForPrisma.prisma;
+const prisma = new PrismaClient();
 
 export default prisma;
