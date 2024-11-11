cc6cb4f6ba00b71146923cac6bb9313633b4e264
tsunakit99 <tainosuke0926@gmail.com>
2024-11-11 11:12:59 +0900

    fix: ビルドエラー解消

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,7 @@ const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
 async function main() {
-  // const departments = ['共通', '知能情報工学科', '情報通信工学科', '知的システム工学科', '物理情報工学科', '生命化学情報工学科']; 
+  // const departments = ['共通', '知能情報工学科', '情報通信工学科', '知的システム工学科', '物理情報工学科', '生命化学情報工学科'];
 
   // for (const name of departments) {
   //   await prisma.department.upsert({
